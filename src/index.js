5d56cd88a587661cea165b4b1bb4385987adf975
Julia Kahn <juliakahn1@gmail.com>
2023-09-17 14:39:21 -0700

    Finish highlighting words with button press

--- src/index.js
+++ src/index.js
@@ -5,17 +5,27 @@ import PartsOfSpeechView from "./scripts/parts-of-speech-view"
 window.PoemView = PoemView
 window.PartsOfSpeechView = PartsOfSpeechView
 
-const poemEl = document.querySelector(".poem");
+// container for title/poet
 const poemMetaData = document.querySelector(".poem-metadata")
-const verbsEl = document.querySelector(".verbs")
+// container for poems
+const poemEl = document.querySelector(".poem");
+// container for parts of speech
+const posEl = document.querySelector(".pos")
+
 
 // testing — CHANGE TO DROPDOWN
 const poem = new PoemView(poemEl, "machado", poemMetaData)
 window.poem = poem
 
-const pos = new PartsOfSpeechView(verbsEl)
+const pos = new PartsOfSpeechView(posEl)
 window.pos = pos
 
 
 // pass in poem that is selected
 // event listener on nav button for poem
+
+
+// EVENTS
+
+// when tile is clicked and unclicked
+    // change the class of the words with the matching class (i.e. verb => highlighted-verb)
--- src/scripts/parts-of-speech-view.js
+++ src/scripts/parts-of-speech-view.js
@@ -1,7 +1,7 @@
 class PartsOfSpeechView {
 
-    constructor(verbsEl) {
-        this.verbsEl = verbsEl
+    constructor(posEl) {
+        this.posEl = posEl
         this.populateWordTags()
     }
 
@@ -9,11 +9,13 @@ class PartsOfSpeechView {
         // should this be a class var?
         const categories = ["noun", "verb", "adjective", "adverb", "article", "pronoun", "preposition", "conjunction"]
         const ul = document.createElement("ul")
-        ul.classList.add("verb-list")
+        ul.classList.add("pos-list")
         categories.forEach(partOfSpeech => {
             ul.append(this.createWordTag(partOfSpeech))
         })
-        this.verbsEl.append(ul)
+        this.posEl.append(ul)
+
+        this.posEl.addEventListener("click", this.highlightWords)
     }
 
 
@@ -24,6 +26,26 @@ class PartsOfSpeechView {
         li.setAttribute("id", `${partOfSpeech}`)
         return li
     }
+
+    // install an event listener
+
+    highlightWords(e) {
+        e.preventDefault()
+
+        // find the class of button pressed
+        const buttonId = e.target.id
+        e.srcElement.classList.toggle("selected")
+        // console.log(e)
+
+        // change color of button — toggle
+
+        const changingWords = document.querySelectorAll(`.${buttonId}`)
+
+        changingWords.forEach(word => {
+            word.classList.toggle("selected")
+        })
+    }
+
 }
 
 export default PartsOfSpeechView;
